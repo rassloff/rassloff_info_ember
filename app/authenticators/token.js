b3959ccf5c2eeeffe8da27c66f7bacb4bc3f9ed6
Rossi <admin@rassloff.info>
2021-09-14 16:57:32 +0200

    so jetzt sollte es passen.

--- app/authenticators/token.js
+++ app/authenticators/token.js
@@ -2,6 +2,7 @@ import Base from 'ember-simple-auth/authenticators/base';
 
 export default Base.extend({
   restore(data) {
+    console.log('??? restore ???');
     console.log(typeof data);
   },
 
@@ -11,17 +12,17 @@ export default Base.extend({
     name = "c1312asd";
     password = "12345asdfghj";
 
-    //const BACKEND_URL = "https://api.rassloff.info/api/v1"; // http://127.0.0.1:8000
+    //const BACKEND_URL = "https://api.rassloff.info/login"; // http://127.0.0.1:8000
 
     const BACKEND_URL = "http://127.0.0.1:8000/login"; // https://api.rassloff.info/api/v1
 
     let response = await fetch( BACKEND_URL, { // '/api/token'
-      mode: 'cors',
+      //mode: 'cors',
       //credentials: 'same-origin',
       //mode: 'no-cors',
       method: 'POST',
-      header: {
-        'Accept': "application/json",
+      headers: {
+        //'Accept': "application/json",
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
@@ -29,7 +30,10 @@ export default Base.extend({
       })
     });
 
-    console.log('??? NO Success:', BACKEND_URL);
+    //console.log('??? NO Success:', BACKEND_URL);
+
+    console.log('??? NO Success:', response);
+
 
     if(response.ok) {
       return response.json();
--- app/controllers/login.js
+++ app/controllers/login.js
@@ -7,18 +7,31 @@ export default class LoginController extends Controller {
   @service session;
 
   @tracked errror;
-  @tracked name;
-  @tracked password;
+  @tracked name = 'c1312asd';
+  @tracked password = '12345asdfghj';
+
+
+
 
   @action
   async login(event) {
+
+    console.log('??? login ???');
+    console.log('??? login ???', this.name);
+    console.log('??? event login ???', event);
+
     event.preventDefault();
     try {
       await this.session.authenticate('authenticator:token', this.name, this.password);
+      //await this.session.authenticate(this.name, this.password);
     } catch(error) {
       this.error = error;
     }
   }
+
+
+
+
 
   @action
   update(attr, event) {
