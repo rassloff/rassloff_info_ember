daa90f69273b415c5b2da8cb8be9da9929dafd36
Rossi <admin@rassloff.info>
2021-09-19 19:49:54 +0200

    yarn upgrade

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,4 +1,29 @@
+import classic from 'ember-classic-decorator';
+import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 import JSONAPIAdapter from '@ember-data/adapter/json-api';
+import config from '../config/environment';
+import { camelize } from '@ember/string';
+import { pluralize } from 'ember-inflector';
 
-export default class ApplicationAdapter extends JSONAPIAdapter {
+@classic
+export default class Application extends JSONAPIAdapter {
+  @service
+  auth;
+
+  coalesceFindRequests = true;
+  host = config.api.host;
+  namespace = config.api.namespace;
+
+  @computed('auth.accessToken')
+  get headers() {
+    let headers = {};
+    headers['Authorization'] = `Bearer ${this.auth.accessToken}`;
+    return headers;
+  }
+
+  pathForType(modelName) {
+    return camelize(pluralize(modelName));
+  }
 }
+
